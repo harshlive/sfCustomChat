f6f2c1eac381d7f4a077f14835c7998cbe3307ac
Subhash <subhash.rajpurohit@aethereus.com>
2020-11-09 11:09:59 +0530

    first

---
+++ app.py
@@ -0,0 +1,135 @@
+# FLASK CHAT APP SALESFORCE LIVEAGENT API
+import requests
+from flask import Flask, request, render_template, Response
+import time
+import json
+from uuid import uuid4
+
+app = Flask(__name__)
+
+keyMap = {}
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/initchat')
+def initchat():
+    sessMapper = str(uuid4())
+    global keyMap
+    session_id, affinity_token, key = getSessionId()
+    getChasitorInit(session_id, affinity_token, key)
+    keyMap[sessMapper] = key
+    return json.dumps({'sessKey': sessMapper, 'affinityToken': affinity_token}), 200, {'ContentType': 'application/json'}
+
+
+@app.route('/stream')
+def stream():
+    global keyMap
+    key = request.args['sessKey']
+    sessKey = keyMap[key]
+    affToken = request.args['affinityToken']
+    # id: <any_id>\nevent: <any_message>\ndata: <any_data>\n\n
+
+    def eventStream():
+        while True:
+            yield f"data: {getMessages(1, affToken, sessKey)}###{key}\n\n"
+        time.sleep(0.5)
+    return Response(eventStream(), mimetype="text/event-stream")
+
+
+@app.route("/sendmessage", methods=['POST'])
+def sendMessage():
+    global keyMap
+    key = request.args['sessKey']
+    sessKey = keyMap[key]
+    affToken = request.args['affinityToken']
+    msg = request.form['message']
+    sendChatMessage(1, affToken, sessKey, msg)
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+def getSessionId():
+    url = "https://d.la2-c2-ukb.salesforceliveagent.com/chat/rest/System/SessionId"
+
+    payload = {}
+    headers = {
+        'X-LIVEAGENT-AFFINITY': 'null',
+        'X-LIVEAGENT-API-VERSION': '50',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_dict = response.json()
+    session_id = response_dict['id']
+    affinity_token = response_dict['affinityToken']
+    key = response_dict['key']
+    return session_id, affinity_token, key
+
+
+def getChasitorInit(session_id, affinity_token, key):
+    url = "https://d.la2-c2-ukb.salesforceliveagent.com/chat/rest/Chasitor/ChasitorInit"
+
+    payload = "{\n  \"sessionId\": \""+session_id+"\",\n  \"organizationId\": \"00D2w000000kcVU\",\n  \"deploymentId\": \"5722w000000UL5h\",\n  \"buttonId\": \"5732w000000UO2n\",\n  \"userAgent\": \"\",\n  \"language\": \"en-US\",\n  \"screenResolution\": \"1900x1080\",\n  \"visitorName\": \"Test Visitor\",\n  \"prechatDetails\": [],\n  \"prechatEntities\": [],\n  \"receiveQueueUpdates\": true,\n  \"isPost\": true\n}"
+
+    headers = {
+        'X-LIVEAGENT-AFFINITY': affinity_token,
+        'X-LIVEAGENT-API-VERSION': '50',
+        'X-LIVEAGENT-SESSION-KEY': key,
+    }
+
+    requests.request("POST", url, headers=headers, data=payload)
+
+
+def getMessages(session_id, affinity_token, key):
+    url = "https://d.la2-c2-ukb.salesforceliveagent.com/chat/rest/System/Messages"
+    payload = {}
+    headers = {
+        'X-LIVEAGENT-AFFINITY': affinity_token,
+        'X-LIVEAGENT-API-VERSION': '50',
+        'X-LIVEAGENT-SESSION-KEY': key,
+
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    json_resp = {}
+    message = ""
+    if response.status_code == 200:
+        json_resp = response.json()
+        if json_resp['messages'][0]['type'] == "ChatMessage":
+            for i in range(0, len(json_resp['messages'])):
+                print("Message array", json_resp['messages'][i]['message'])
+                if 'text' in json_resp['messages'][i]['message'].keys():
+                    message += json_resp['messages'][i]['message']['text']
+        elif json_resp['messages'][0]['type'] == "ChatRequestSuccess":
+            message = "Connected to our agent"
+        else:
+            message = "Typing"
+    else:
+        print(response.status_code)
+        message = "Typing"
+
+    print("Message: ", message)
+    return message
+
+
+def sendChatMessage(session_id, affinity_token, key, message):
+    url = "https://d.la2-c2-ukb.salesforceliveagent.com/chat/rest/Chasitor/ChatMessage"
+    payload = '{"text": "' + message + '"}'
+
+    headers = {
+        'X-LIVEAGENT-AFFINITY': affinity_token,
+        'X-LIVEAGENT-API-VERSION': '50',
+        'X-LIVEAGENT-SESSION-KEY': key,
+
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.status_code
+
+
+if __name__ == "__main__":
+    app.run()
+
+# Endpoint https://d.la2-c2-ukb.salesforceliveagent.com/chat/rest/
